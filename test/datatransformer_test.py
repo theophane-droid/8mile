2266b7c24b8b45d274eb32e565173bd8c485258e
theophanedroid <theodesile@gmail.com>
2022-10-19 15:08:14 +0000

    update AE test

--- test/datatransformer_test.py
+++ test/datatransformer_test.py
@@ -1,30 +1,21 @@
+import os
 import unittest
 from Hmile.DataProvider import CSVDataProvider, ElasticDataProvider
 from Hmile.FillPolicy import FillPolicyAkima
 from Hmile.DataTransformer import TaFeaturesTransformer
 from Hmile.utils import trainAE
 
-ELASTIC_USER=''
-ELASTIC_PASS=''
-ELASTIC_URL = ''
 class TestTransformer(unittest.TestCase):
     
     def setUp(self):
-        self.dp = ElasticDataProvider('BTCUSD', '2020-01-01', '2022-01-03', es_url=ELASTIC_URL, es_user=ELASTIC_USER, es_pass=ELASTIC_PASS, interval='hour')
+        self.elastic_url = os.environ['ELASTIC_URL']
+        self.elastic_user = os.environ['ELASTIC_USER']
+        self.elastic_pass = os.environ['ELASTIC_PASS']
+        self.dp = ElasticDataProvider('BTCUSD', '2020-01-01', '2022-01-03', es_url=self.elastic_url, es_user=self.elastic_user, es_pass=self.elastic_pass, interval='hour')
         self.dp.fill_policy = FillPolicyAkima('hour')
         self.transformer = TaFeaturesTransformer(self.dp)
-        
-    def test(self):
-        nb_col = self.transformer.transform().shape[1]
-        print(type(self.transformer.transform()))
-        self.assertEqual(nb_col, 493)
+
     def test_trainAE(self) :
         df = self.transformer.transform()
         AE = trainAE(df,nb_epoch=10)
         self.assertIsNotNone(AE)
-
-        
-
-
-if __name__ == '__main__':
-    unittest.main()
